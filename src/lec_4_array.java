97fcd1c07683d4d2f601b4419c3c0a467419c739
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-03-26 11:19:29 +0600

    lec_4_array updated

---
+++ src/lec_4_array.java
@@ -0,0 +1,13 @@
+public class lec_4_array {
+    public static void main(String[] args){
+        //syntax : datatype[] array_name = {elements}
+
+        int[] arr1 = {1,2,3,4};
+        System.out.println(arr1);//display array address
+        System.out.println(arr1[0]);//display first index(first element)
+        //user can display all elements using loop
+        for(int i =0;i<4;i++){
+            System.out.println(arr1[i]);
+        }
+    }
+}
