3e64cbd7566841a10e357f82dd52f92197262e79
fayjullah haque <fayjullah.h.emon@gmail.com>
2026-03-26 11:55:22 +0600

    update milestone_2 , lec_1_operator file updated with arithmetic operators

---
+++ src/milestone_2/lec_1_operator.java
@@ -0,0 +1,20 @@
+package milestone_2;
+
+public class lec_1_operator {
+    public static void main(String[] args) {
+        System.out.println("Arithmetic operator");
+        int a = 5;
+        int b = 4;
+        System.out.println("Sum of a+b = "+ (a+b));
+        System.out.println("Substraction of a-b = "+ (a-b));
+        System.out.println("Multiplication of a x b = "+ (a*b));
+        System.out.println("Division of a / b = "+ (a/b));
+        System.out.println("Modulus of a % b = "+ (a%b));
+        System.out.println("increment of a  = "+ (a++));
+        System.out.println("increment of b  = "+ (++b));
+        System.out.println("decrement of a  = "+ (a--));
+        System.out.println("decrement of b  = "+ (--b));
+
+//        System.out.println("Arithmetic operator");
+    }
+}
